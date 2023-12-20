7a81b69418645d7aa8213336dc9583eada620162
Geo Bouwmeester <geo.bouwmeester@student.hu.nl>
2023-12-20 20:59:14 +0100

    Day 1 Part 2

--- main.py
+++ main.py
@@ -13,3 +13,33 @@ for item in data:
     total += item[-1]
 
 print("Part 1 solution: ", total) 
+
+
+with open("input.txt") as file:
+    data = []
+
+    for line in file.readlines():
+        line = line.replace('one', 'o1ne')
+        line = line.replace('two', 't2wo')
+        line = line.replace('three', 't3hree')
+        line = line.replace('four', 'f4our')
+        line = line.replace('five', 'f5ive')
+        line = line.replace('six', 's6ix')
+        line = line.replace('seven', 's7even')
+        line = line.replace('eight', 'e8ight')
+        line = line.replace('nine', 'n9ine')
+        line = line.replace('zero', 'z0ero')
+        item = []
+
+        for character in line:
+            if character >= '0' and character <= '9':
+                item.append(int(character))
+        data.append(item)
+
+total = 0 
+
+for item in data:
+    total += (10 * item[0])
+    total += item[-1]
+
+print("Part 2 solution:", total)
